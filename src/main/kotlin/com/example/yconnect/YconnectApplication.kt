9c9a9e47bf02063959c15c81efbe197684dcfd81
keisuke <keisuke_main@keisukenoMacBookPro-2080tiai.local>
2023-08-14 23:19:52 +0900

    :sparkles: init commit

---
+++ src/main/kotlin/com/example/yconnect/YconnectApplication.kt
@@ -0,0 +1,11 @@
+package com.example.yconnect
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YconnectApplication
+
+fun main(args: Array<String>) {
+	runApplication<YconnectApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/yconnect/YconnectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.yconnect
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YconnectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
